ea510ad1d7893a11c99972bd17f27d1746c6b097
amit bhatt <amitfreeze@gmail.com>
2026-03-28 11:21:12 +0530

    python day3

---
+++ sample.py
@@ -0,0 +1,6 @@
+from faker import Faker
+
+fake = Faker()  # Create a Faker generator
+print(f"Fake Name: {fake.name()}")
+print(f"Fake Address: {fake.address()}")
+print(f"Fake Text: {fake.text()}")
